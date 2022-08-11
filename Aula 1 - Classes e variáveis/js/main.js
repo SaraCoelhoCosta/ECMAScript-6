130e34128b7943a4808a3bd7447ba80c17859342
SaraCoelhoCosta <costa.saracoelho@gmail.com>
2022-08-11 20:00:37 -0300

    Introdução a ES 6 - Classes e variáveis

--- Aula 1 - Classes e variáveis/js/main.js
+++ Aula 1 - Classes e variáveis/js/main.js
@@ -1,3 +1,5 @@
+// Classes
+
 class List { // Classe (Pai)
 
     constructor() { // Construtor da classe
@@ -28,14 +30,17 @@ class ToList extends List { // Classe com herança (Filho)
 
 const toList = new ToList() // Instância
 
-document.getElementById("novoToDo").onclick = function (){ // Ao clicar no botão, vai chamar uma ação
+document.getElementById("novoToDo").onclick = function () { // Ao clicar no botão, vai chamar uma ação
     toList.add('Item'); // Chamada da função passando como parâmetro 'Item'
     toList.getUsuario(); // Chamada da função
 }
 
 console.log(ToList.soma(5, 8)); // Imprime a soma no console, chamando a função estática (resultado: 13)
 
+/* --------------------------------------------------------------------------------------------------------------------------- */
+
 // Tipos de variáveis
+
 var tipo_var = 'Variável pode ser alterada em qualquer momento'; // Pode ser alterada (global)
 
 const tipo_const = 'Constante não pode ser modificada após criada'; // Não pde ser alterada (global)
@@ -45,7 +50,7 @@ tipo_const2.texto = 'Propriedade alterada';
 console.log(tipo_const2)
 
 function testadoTipoVariavel(valor1) {
-    let valor2  = 4; // Só é vista dentro da função (local)
+    let valor2  = 4; // Só é vista dentro da função (local - usada na função)
     if (valor1 >= 2) { // Estrutura condicional
         valor2 = 6;
         console.log(valor1, valor2);
